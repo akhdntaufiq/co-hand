b38776d567f3cb82926dfb0d49bd3d161ed174cc
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2024-09-09 15:35:09 +0700

    pws url update

--- co_hand/settings.py
+++ co_hand/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)66gs6yv^whsd823vmnb3rmyft-d1zqnumdsd=-pwp1=yvz3ly
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "akhdan-taufiq-cohand.pbp.cs.ui.ac.id"]
 
 
 # Application definition
